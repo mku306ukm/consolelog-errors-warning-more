7cb5e808713c11187cf99aed9717bc31ad246767
samod singh <samod0098@gmail.com>
2021-03-11 14:33:04 +0530

    consolelog-errors-warning-more

---
+++ test.js
@@ -0,0 +1,16 @@
+
+console.time('my code took');
+console.log('hello console');
+console.log(5+25);
+console.log(34);
+console.log([34,2,1,2]);
+console.table({ram: 'this',marks:34});
+console.warn('this is warning');
+console.timeEnd('my code took'); 
+console.error('this is an error');
+
+/*
+this
+is a
+multiline comment
+*/
